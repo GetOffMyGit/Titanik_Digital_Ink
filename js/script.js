1a2096258b59c5025da8da96a41b89e11da979c3
PotatoMashTW <mwtw1994@livemail.tw>
2016-10-13 15:32:32 +1300

    Added colour palette. (Reupload)

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
         maxWidth: 1,
         penColor: "rgb(66, 133, 244)"
     });
-
+	
     drawingPad.on();
 	
     var deselectButton = $("#deselectButton")[0];
-	var undoButton = $("#undoButton")[0];
+    var undoButton = $("#undoButton")[0];
     var saveButton = $("#saveButton")[0];
     var loadButton = $("#loadButton")[0];
     var downloadRef = $("#downloadLink")[0];
@@ -18,90 +18,90 @@ $(document).ready(function() {
     var circleShapeButton = $("#circleShapeButton")[0];
     var squareShapeButton = $("#squareShapeButton")[0];
     var triangleShapeButton = $("#triangleShapeButton")[0];
-
+	
     $("#downloadLink").hide();
 	
-    deselectButton.addEventListener("click", function () {
-		drawingPad.deselectShapes();
+	// simpleColorPicker functions
+	$('select[name="colorPicker"]').on('change', function() {
+		drawingPad.setColour($('select[name="colorPicker"]').val());
 	});
-
-	undoButton.addEventListener("click", function () {
-		drawingPad.undo();
+	$('select[name="colorPickerBackground"]').on('change', function() {
+		$(document.body).css('background-color', $('select[name="colorPickerBackground"]').val());
 	});
+	$('select[name="colorPicker"]').simplecolorpicker({picker: true, theme: 'glyphicons'});
+    $('select[name="colorPickerBackground"]').simplecolorpicker();
 	
-	redoButton.addEventListener("click", function () {
-		drawingPad.redo();
-	});
-	
-	clearButton.addEventListener("click", function () {
-	swal({
-		title: "Are you sure?",
-		text: "You will not be able to recover any drawings!",
-		type: "warning",
-		showCancelButton: true,
-		confirmButtonColor: "#DD6B55",
-		confirmButtonText: "Yes, clear it!",
-		closeOnConfirm: false
-	},function(){
-		drawingPad.clear();
-		drawingPad.clearStack();
-		swal("Cleared!", "Your drawing pad has been reset.", "success");
-		});		
-	});
-	
-    saveButton.addEventListener("click", function () {
+	// button functions
+    deselectButton.addEventListener("click", function() {
+        drawingPad.deselectShapes();
+    });
+	undoButton.addEventListener("click", function() {
+        drawingPad.undo();
+    });
+	redoButton.addEventListener("click", function() {
+        drawingPad.redo();
+    });
+	clearButton.addEventListener("click", function() {
+        swal({
+            title: "Are you sure?",
+            text: "You will not be able to recover any drawings!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, clear it!",
+            closeOnConfirm: false
+        }, function() {
+            drawingPad.clear();
+            drawingPad.clearStack();
+            swal("Cleared!",
+                "Your drawing pad has been reset.",
+                "success");
+        });
+    });
+	saveButton.addEventListener("click", function() {
         var inkLines = drawingPad.getListOfShapes();
-
         var json = JSON.stringify(inkLines);
-        var blob = new Blob([json], {type: "application/json"});
-        var url  = URL.createObjectURL(blob);
-
+        var blob = new Blob([json], {
+            type: "application/json"
+        });
+        var url = URL.createObjectURL(blob);
         $("#downloadLink").show();
         downloadRef.download = "inks.json";
-        downloadRef.href        = url;
+        downloadRef.href = url;
         downloadRef.textContent = "Download inks.json";
     });
-
-    loadButton.addEventListener("click", function () {
+	loadButton.addEventListener("click", function() {
         //TODO: load from external chosen file
-
         // no input while loading ink
         drawingPad.off();
         $.getJSON("inks.json", function(json) {
             // iterate through all the lines
-            for(var i = 0; i < json.length; i++) {
+            for (var i = 0; i < json.length; i++) {
                 var line = json[i];
-
                 // draw the line given by points
                 drawingPad.drawFromJson(line);
             }
-
             // allow ink after input done
             drawingPad.on();
         });
     });
-
-    
+	
     // keeps one draw tool always "active"
-    $(".draw-tool").click(function(){
+    $(".draw-tool").click(function() {
         $(".draw-tool").removeClass("active");
         $(this).addClass("active");
     });
-
-    penButton.addEventListener("click", function () {
+	penButton.addEventListener("click", function() {
         drawingPad.setMode(0);
     });
-
-    circleShapeButton.addEventListener("click", function () {
+	circleShapeButton.addEventListener("click", function() {
         drawingPad.setMode(1);
     });
-
-    squareShapeButton.addEventListener("click", function () {
+	squareShapeButton.addEventListener("click", function() {
         drawingPad.setMode(2);
     });
-
-    triangleShapeButton.addEventListener("click", function () {
+	triangleShapeButton.addEventListener("click", function() {
         drawingPad.setMode(3);
     });
+	
 });
-
